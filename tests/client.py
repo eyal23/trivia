9d6764b0455df3bc2a6121ce75b43a874aea2453
eyal23 <eyal.heinrich.2003@gmail.com>
2020-05-04 16:01:13 +0300

    added createSocket function, completed the test

---
+++ tests/client.py
@@ -0,0 +1,31 @@
+import socket
+
+def createSocket():
+    port = 0
+
+    while port < 1024 or port > 65535:
+        port = int(input("Enter the port: "))
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverAddress = ("127.0.0.1", port)
+    sock.connect(serverAddress)
+
+    return sock
+
+def main():
+    isConnected = True
+
+    try:
+        sock = createSocket()
+    except:
+        isConnected = False
+        print("the server is currently down...")
+
+    if isConnected:
+        while True:
+            response = sock.recv(1024)
+            if response == "Hello":
+                sock.sendall(response)
+
+if __name__ == '__main__':
+    main()
