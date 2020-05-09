2c856e74ae54eef361cc10d80fa3a89167eca4db
eyal23 <eyal.heinrich.2003@gmail.com>
2020-05-09 13:08:39 +0300

    completed all functions of the client test, only description left

--- tests/client.py
+++ tests/client.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 """
     usage: the function creates the client socket
@@ -17,6 +18,40 @@ def createSocket():
 
     return sock
 
+def constructMessage(code, jsonData):
+    bsonData = json.dumps(jsonData).encode("utf-8")
+
+    message = bytearray()
+    message.append(code)
+    message.append(len(bsonData))
+
+    for byte in bsonData:
+        message.append(byte)
+
+    return message
+
+def sendAndRecive(sock, message):
+    sock.sendall(message)
+    response = sock.recv(1024).deocde("utf-8")
+    print(response)
+
+def sendLoginRequest(sock):
+    jsonData = {
+        "username": "user1",
+        "password": "1234"
+    }
+
+    sendAndRecive(sock, constructMessage(1, jsonData))
+
+def sendSingUpRequest(sock):
+    jsonData = {
+        "username": "user1",
+        "password": "1234",
+        "email": "user1@gmail.com"
+    }
+
+    sendAndRecive(sock, constructMessage(2, jsonData))
+
 def main():
     isConnected = True
 
@@ -30,6 +65,17 @@ def main():
         response = sock.recv(1024).decode()
         if response == "Hello":
             sock.sendall(response.encode())
+        while True:
+            choice = input("""1. login request
+            2. sign up request
+            enter you choice: """)
+
+            if choice == 1:
+                sendLoginRequest(sock)
+            elif choice == 2:
+                sendSingUpRequest(sock)
+            else:
+                print("ilegal choice!")
 
 if __name__ == '__main__':
     main()
