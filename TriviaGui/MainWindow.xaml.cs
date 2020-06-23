c1234110165e54b196e58c01f218ff0e57529439
liran eliyahu <liranpro54@gmail.com>
2020-06-23 22:50:47 +0300

    these are few of the screens, the other ones will be added shortly

---
+++ TriviaGui/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TriviaGui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool isOnRegister = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            isOnRegister = !isOnRegister;
+            var temp = login.Content;
+            login.Content = (string)signup.Content;
+            signup.Content = temp;
+
+            userName.Text = "";
+            password.Password = "";
+
+            mainManu main = new mainManu();
+            main.Show();
+            this.Close();
+        }
+
+        private void signUp_Click(object sender, RoutedEventArgs e)
+        {
+            if (isOnRegister)
+                MessageBox.Show($"send: {userName.Text} {password.Password}");
+
+
+            else
+                MessageBox.Show($"send: {userName.Text} {password.Password}");
+        }
+
+        private void MainWindow_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.WindowState = WindowState.Minimized;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ TriviaGui/mainManu.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TriviaGui
+{
+    /// <summary>
+    /// Interaction logic for mainManu.xaml
+    /// </summary>
+    public partial class mainManu : Window
+    {
+        public mainManu()
+        {
+            InitializeComponent();
+        }
+        private void mainManu_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.WindowState = WindowState.Minimized;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void maxButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void StatisticsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateRoomButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void JoinRoomButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
