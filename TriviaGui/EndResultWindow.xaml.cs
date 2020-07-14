b92328853543afe9f918c5e88bc16a9562047f59
liran eliyahu <liranpro54@gmail.com>
2020-07-14 04:01:25 +0300

    added the needed windows, updated some old windows & pages

---
+++ TriviaGui/EndResultWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TriviaGui
+{
+    /// <summary>
+    /// Interaction logic for EndResultWindow.xaml
+    /// </summary>
+    public partial class EndResultWindow : Window
+    {
+        public EndResultWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
